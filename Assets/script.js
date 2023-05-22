ecc7bc892d42e34d67bb39f163943fdfed9d337b
Ethan <131805068+corynko@users.noreply.github.com>
2023-05-22 14:48:01 -0600

    Added javascript giving functionality to pwd generator. Wrote README

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,70 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+var defaultSet = "abcdefghijklmnopqrstuvwxyz";
+var capSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var specKeys = ",./;'<>?:|!@#$%^&*-_=+";
+var newSet = defaultSet;
+var characterCount = 8;
 var generateBtn = document.querySelector("#generate");
+var password = "";
+var passwordText = document.querySelector("#password");
+
+//prompt method - prompt with dialogue window
+function userPrompts() {
+  characterCount = window.prompt(
+    "How Many Characters Would You Like to Include, Up to 128?"
+  );
+  if (characterCount > 128) {
+    characterCount = window.prompt("Please Enter a Number Between 1 and 128");
+  }
+  if (characterCount > 128) {
+    characterCount = 128;
+  }
+  var caps = window.confirm("Would You Like to Include Capitals?");
+  var num = window.confirm("Would You Like to Include Numbers?");
+  var specs = window.confirm("Would You Like to Include Special Characters?");
+
+  if (caps === true) {
+    newSet += capSet;
+  }
+  if (num === true) {
+    newSet += numbers;
+  }
+  if (specs === true) {
+    newSet += specKeys;
+  }
+
+  generatePassword();
+  // console.log(newSet);
+  // console.log(characterCount);
+}
+
+//Possible HTML method(?) - show checkboxes sequentially
+// var capsBox = document.querySelector("#capsBox");
+// var specBox = document.querySelector("#specBox");
+// var numBox = document.querySelector("#numBox");
+
+function generatePassword() {
+  password = "";
+  for (var i = 0; i < characterCount; i++) {
+    password += newSet.charAt(Math.floor(Math.random() * newSet.length));
+  }
+
+  writePassword();
+
+  //test within generate function
+  // console.log(newSet);
+  // console.log(characterCount);
+  // console.log(password);
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  passwordText.textContent = password;
+  //final test
+  // console.log(newSet);
+  // console.log(characterCount);
+  // console.log(password);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+generateBtn.addEventListener("click", userPrompts);
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Password-Generator
+
+## What Is It?
+
+This nifty little website can generate a truly random string up to 128 characters long, creating passwords that can stand up to brute force attacks for hundreds of years.
+
+## Where Is It?
+
+You can view (and use) the generator at [https://corynko.github.io/Password-Generator](https://corynko.github.io/Password-Generator)
+
+## How Do I Use It?
+
+Click the "Generate Password" button, and answer the prompts. Your new password will appear in the box.
+
+## Who Can Use It?
+
+This generator is open source and based on starter code by @friendly-parakeet. Anyone can use it, but please note that no actual cybersecurity experts have verified the claims made in this README.
